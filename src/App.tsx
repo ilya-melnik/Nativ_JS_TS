7ba0aa0d88139ddaeabf124f161742bf25d9547f
lya-melnik <ilay.melnik.kr@gmail.com>
2022-11-29 14:27:46 -0800

    06 callback, onClick, onChange. удаляеться текст при нажатии btn, value is added to the page when the button is clicked

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import {Touppercase} from "./01-hello-tests/01";
 import {student, StudentType} from "./02-object-test/02";
 import {Filter} from "./04-filter/04";
 import {Map} from "./05-map/05";
+import Callback from "./06-callBack-onClick-onChange-onBlure/callback";
 
 function App() {
     console.log(student)
   return (
     <div className="App">
-<Touppercase  value='How are you ?'/>
-        <Filter />
-        <Map />
+{/*<Touppercase  value='How are you ?'/>*/}
+        {/*<Filter />*/}
+        {/*<Map />*/}
+        <Callback />
     </div>
   );
 }
